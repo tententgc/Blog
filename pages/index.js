64be1b40fe9f99c04300845aa8876879a044001a
Thanyapisit Buaprakong <nyman3527@gmail.com>
2022-07-12 19:47:01 +0700

    update Blog css

---
+++ pages/index.js
@@ -0,0 +1,70 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { GraphQLClient, gql } from "graphql-request";
+import BlogCard from "../components/BlogCard";
+
+const graphcms = new GraphQLClient(
+  "https://api-ap-northeast-1.graphcms.com/v2/cl5gnhwto452701uh6w2k17ws/master"
+);
+
+const QUERY = gql`
+  {
+    posts {
+      id
+      title
+      datePublished
+      slug
+      content {
+        html
+      }
+      author {
+        name
+        avatar {
+          url
+        }
+      }
+      coverPhoto {
+        publishedAt
+        createdBy {
+          id
+        }
+        url
+      }
+    }
+  }
+`;
+
+export async function getStaticProps() {
+  const { posts } = await graphcms.request(QUERY);
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 30,
+  };
+}
+
+export default function Home({ posts }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Digital Scribbles</title>
+        <meta name="description" content="A blog tutorial made with JAMstack" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        {posts.map((post) => (
+          <BlogCard
+            title={post.title}
+            author={post.author}
+            coverPhoto={post.coverPhoto}
+            key={post.id}
+            datePublished={post.datePublished}
+            slug={post.slug}
+          />
+        ))}
+      </main>
+    </div>
+  );
+}
